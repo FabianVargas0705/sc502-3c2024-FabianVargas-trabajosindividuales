503d0b66dd06115481665d5b7589643497b23a20
Fabian vargas <105955465+FabianVargas0705@users.noreply.github.com>
2025-02-03 13:41:51 -0600

    fabian vargas
    Commit tarea uno

---
+++ practica-programada-1/segunda-parte/script.js
@@ -0,0 +1,5 @@
+document.getElementById('form-agenda').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Cita reservada con éxito');
+    window.location.href = 'index.html'; 
+});
